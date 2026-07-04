2ab46014e0b63e95952502c503151858aa1b1d17
Erik <atsireopac@gmail.com>
2026-07-03 23:50:42 -0300

    feat: criar backend Spring Boot inicial

---
+++ backend/src/main/java/br/gov/ife/sgrsc/SgRscBackendApplication.java
@@ -0,0 +1,12 @@
+package br.gov.ife.sgrsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgRscBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgRscBackendApplication.class, args);
+    }
+}
