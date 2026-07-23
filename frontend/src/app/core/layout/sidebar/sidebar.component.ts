2077d96b677e7d3d278b6bd3f748301f31fb2c87
atsireopac <atsireopac@gmail.com>
2026-07-23 19:50:28 -0300

    refactor(sidebar): gerar menu dinamicamente com @for

--- frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatListModule,
+    MatIconModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  readonly menuItems = [
+    {
+      label: 'Dashboard',
+      icon: 'dashboard',
+      route: '/',
+      exact: true
+    },
+    {
+      label: 'Solicitações',
+      icon: 'description',
+      route: '/solicitacoes',
+      exact: false
+    },
+    {
+      label: 'Documentos',
+      icon: 'folder',
+      route: '/documentos',
+      exact: false
+    },
+    {
+      label: 'Pareceres',
+      icon: 'fact_check',
+      route: '/pareceres',
+      exact: false
+    },
+    {
+      label: 'Administração',
+      icon: 'settings',
+      route: '/administracao',
+      exact: false
+    }
+  ];
 
 }
